ccd48e65207e0d16455cc7315306ffd34feb2b1d
Aurélien <aurel.andre@yahoo.fr>
2016-02-27 15:45:18 +0100

    Version avec bug texture

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "tgaimage.h"
 #include "point.h"
+#include "matrice.h"
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <cmath>
+#include <algorithm>
+
+#define M_PI  3.14159265358979323846
 
 using namespace std;
 
@@ -10,6 +15,7 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor blue = TGAColor(159, 121, 238, 255);
 const TGAColor purple = TGAColor(155, 48, 255, 255);
+int zbuffer[1000][1000];
 
 
 /* Premiere partie : Lecture du fichier OBJ puis création du TGA (affichage du portait en nuage de point et fil de fer */
@@ -418,7 +424,7 @@ void lineSweeping(vector<vector<double> > &vectPoints, vector<vector<double> > &
 		C.setX(x3);
 		C.setY(y3);
 
-		triangle(A, B, C, image, red);
+		triangle(A, B, C, image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
 	}
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
@@ -426,15 +432,324 @@ void lineSweeping(vector<vector<double> > &vectPoints, vector<vector<double> > &
 	cout << "Succes" << endl;
 }
 
+/* Troisieme partie : On dessine que les triangles de devant et on ajoute une teinte */
+
+void faceLumiere(vector<vector<double> > &vectPoints, vector<vector<double> > &vectTriangles){
+	cout << "Creation image triangle (face visible seulement)... ";
+	int tailleImage = 1000;
+	int tailleImageSurDeux = tailleImage / 2;
+	TGAImage image(tailleImage, tailleImage, TGAImage::RGB);
+
+	int ligne1, ligne2, ligne3;
+	int Ax, Ay, Az, Bx, By, Bz, Cx, Cy, Cz, maxAbs, minAbs, maxOrd, minOrd;
+	double xLumiere = 0;
+	double yLumiere = 0;
+	double zLumiere = 1;
+	double tmp = sqrt(xLumiere*xLumiere + yLumiere*yLumiere + zLumiere*zLumiere);
+	xLumiere /= tmp;
+	yLumiere /= tmp;
+	zLumiere /= tmp;
+
+
+	for (int i = 0; i<1000; i++) {
+		for (int j = 0; j<1000; j++) {
+			zbuffer[i][j] = std::numeric_limits<int>::min();
+		}
+	}
+
+	for (unsigned int i = 0; i < vectTriangles.size(); ++i) {
+		ligne1 = vectTriangles[i][0];
+		Ax = vectPoints[ligne1 - 1][0] + tailleImageSurDeux;
+		Ay = vectPoints[ligne1 - 1][1] + tailleImageSurDeux;
+		Az = vectPoints[ligne1 - 1][2] + tailleImageSurDeux;
+
+		ligne2 = vectTriangles[i][1];
+		Bx = vectPoints[ligne2 - 1][0] + tailleImageSurDeux;
+		By = vectPoints[ligne2 - 1][1] + tailleImageSurDeux;
+		Bz = vectPoints[ligne2 - 1][2] + tailleImageSurDeux;
+
+		ligne3 = vectTriangles[i][2];
+		Cx = vectPoints[ligne3 - 1][0] + tailleImageSurDeux;
+		Cy = vectPoints[ligne3 - 1][1] + tailleImageSurDeux;
+		Cz = vectPoints[ligne3 - 1][2] + tailleImageSurDeux;
+
+		maxAbs = maxTrois(Ax, Bx, Cx);
+		minAbs = minTrois(Ax, Bx, Cx);
+		maxOrd = maxTrois(Ay, By, Cy);
+		minOrd = minTrois(Ay, By, Cy);
+
+		int xAB = Bx - Ax;
+		int yAB = By - Ay;
+		int zAB = Bz - Az;
+
+		int xAC = Cx - Ax;
+		int yAC = Cy - Ay;
+		int zAC = Cz - Az;
+
+		double xNormal = yAB*zAC - zAB*yAC;
+		double yNormal = zAB*xAC - xAB*zAC;
+		double zNormal = xAB*yAC - yAB*xAC;
+
+		double tmp = sqrt(xNormal*xNormal + yNormal*yNormal + zNormal*zNormal);
+		xNormal /= tmp;
+		yNormal /= tmp;
+		zNormal /= tmp;
+
+		float denominateur = (((Bx - Ax)*(Cy - Ay)) - ((Cx - Ax)*(By - Ay)));
+
+
+		float coeff = 1. / denominateur;
+
+		//cout << xNormal << " " << yNormal << " " << zNormal << " light: " << xLumiere << " " << yLumiere << " " << zLumiere << endl;
+
+		double intensity = max(0., xNormal*xLumiere + yNormal*yLumiere + zNormal*zLumiere);
+		
+		//cout << intensity << endl;
+		TGAColor color = TGAColor(intensity * 255, intensity * 255, intensity* 255, 255);
+		for (int i = minOrd; i <= maxOrd; i++) {
+			for (int j = minAbs; j <= maxAbs; j++) {
+				int Px = j;
+				int Py = i;
+				float u = (coeff*(Cy - Ay))*(Px - Ax) + (coeff*(Ax - Cx))*(Py - Ay);
+				float v = (coeff*(Ay - By))*(Px - Ax) + (coeff*(Bx - Ax))*(Py - Ay);
+				float w = 1 - u - v;
+				if (u < -1e-5 || v < -1e-5 || w < -1e-5) {
+				}
+				else {
+					int Pz = u*Az + v*Bz + w*Cz;
+					if (zbuffer[Px][Py] > Pz) {
+						continue;
+					}
+					else {
+						zbuffer[Px][Py] = Pz;
+						image.set(Px, Py, color);
+					}
+				}
+			}
+		}
+	}
+	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	image.write_tga_file("lineFace.tga");
+	cout << "Succes" << endl;
+}
+
+vector<vector<double> > lectureTexturesF(string nomFichier) {
+	cout << "Lecture des textures f (partie 1)... ";
+	ifstream fichier(nomFichier, ios::in);  // on ouvre en lecture
+
+	if (fichier)  // si l'ouverture a fonctionné
+	{
+		string ligne;
+		vector<vector<double> > vect;
+		vector<double> vectLigne;
+		int nbTexture = 0;
+		//int test = 0;
+		while (getline(fichier, ligne, '\n'))  // tant que l'on peut mettre la ligne dans "contenu"
+		{
+			/*if (test >= 5)
+				break;*/
+			if (ligne[0] == 'f' && ligne[1] == ' ')
+			{
+				//test++;
+				string aux;
+				for (unsigned int i = 2; i < ligne.size(); ++i)
+				{
+					aux.push_back(ligne[i]);
+				}
+
+				int posEspace1 = aux.find(' ', 0);
+				int posEspace2 = aux.find(' ', posEspace1 + 1);
+
+				string texte1 = aux.substr(0, posEspace1);
+				string texte2 = aux.substr(posEspace1 + 1, posEspace2 - (posEspace1 + 1));
+				string texte3 = aux.substr(posEspace2 + 1, aux.size() - (posEspace2 + 1));
+
+				int posSlash1Texte1 = texte1.find('/', 0);
+				int posSlash1Texte2 = texte2.find('/', 0);
+				int posSlash1Texte3 = texte3.find('/', 0);
+
+				double partie1 = atof(texte1.substr(posSlash1Texte1+1, posSlash1Texte1).c_str());
+				double partie2 = atof(texte2.substr(posSlash1Texte2+1, posSlash1Texte2).c_str());
+				double partie3 = atof(texte3.substr(posSlash1Texte3+1, posSlash1Texte3).c_str());
+
+				//cout << partie1 << " | " << partie2 << " | " << partie3 << endl;
+
+				vectLigne.push_back(partie1);
+				vectLigne.push_back(partie2);
+				vectLigne.push_back(partie3);
+				vect.push_back(vectLigne);
+
+				nbTexture++;
+			}
+			vectLigne.clear();
+		}
+		cout << "(" << nbTexture << " textures f) ";
+		cout << "Succes" << endl;
+		return vect;
+	}
+	else
+		cerr << "Impossible d'ouvrir le fichier !" << endl;
+}
+
+vector<vector<double> > lectureTexturesVt(string nomFichier) {
+	cout << "Lecture des textures vt (partie 2)... ";
+	ifstream fichier(nomFichier, ios::in);  // on ouvre en lecture
+
+	if (fichier)  // si l'ouverture a fonctionné
+	{
+		string ligne;
+		vector<vector<double> > vect;
+		vector<double> vectLigne;
+		int nbTexture = 0;
+		//int test = 0;
+		while (getline(fichier, ligne, '\n'))  // tant que l'on peut mettre la ligne dans "contenu"
+		{
+			/*if (test >= 5)
+				break;*/
+			if (ligne[0] == 'v' && ligne[1] == 't' && ligne[2] == ' ' && ligne[3] == ' ')
+			{
+				//test++;
+				string aux;
+				for (unsigned int i = 4; i < ligne.size(); ++i)
+				{
+					aux.push_back(ligne[i]);
+				}
+
+				stringstream ss;
+				ss.str(aux);
+				double partie1, partie2;
+				ss >> partie1 >> partie2;
+
+				//cout << partie1 << " | " << partie2 << endl;
+
+				vectLigne.push_back(partie1);
+				vectLigne.push_back(partie2);
+				vect.push_back(vectLigne);
+
+				nbTexture++;
+			}
+			vectLigne.clear();
+		}
+		cout << "(" << nbTexture << " textures vt) ";
+		cout << "Succes" << endl;
+		return vect;
+	}
+	else
+		cerr << "Impossible d'ouvrir le fichier !" << endl;
+}
+
+void faceTexture(vector<vector<double> > &vectPoints, vector<vector<double> > &vectTriangles, vector<vector<double> > &vectTexturesF, vector<vector<double> > &vectTexturesVt) {
+	cout << "Creation image texture... ";
+	int tailleImage = 1000;
+	int tailleImageSurDeux = tailleImage / 2;
+	TGAImage image(tailleImage, tailleImage, TGAImage::RGB);
+
+	TGAImage africanDiffuse;
+	africanDiffuse.read_tga_file("african_head_diffuse.tga");
+	africanDiffuse.flip_vertically();
+	
+	TGAImage test(tailleImage, tailleImage, TGAImage::RGB);
+
+	double ligne1, ligne2, ligne3, Ax, Ay, Az, Bx, By, Bz, Cx, Cy, Cz, maxAbs, minAbs, maxOrd, minOrd, fA, fB, fC;
+	int pix_x, pix_y;
+	double vtAx, vtAy, vtBx, vtBy, vtCx, vtCy;
+	TGAColor colorPix;
+
+	for (int i = 0; i<1000; i++) {
+		for (int j = 0; j<1000; j++) {
+			zbuffer[i][j] = std::numeric_limits<int>::min();
+		}
+	}
+	
+	for (unsigned int i = 0; i < 2; ++i) {
+		ligne1 = vectTriangles[i][0];
+		Ax = vectPoints[ligne1 - 1][0] + tailleImageSurDeux;
+		Ay = vectPoints[ligne1 - 1][1] + tailleImageSurDeux;
+		Az = vectPoints[ligne1 - 1][2] + tailleImageSurDeux;
+		fA = vectTexturesF[i][0];
+		vtAx = vectTexturesVt[fA - 1][0];
+		vtAy = vectTexturesVt[fA - 1][1];
+		TGAColor colorA = africanDiffuse.get(vtAx, vtAy);
+
+		ligne2 = vectTriangles[i][1];
+		Bx = vectPoints[ligne2 - 1][0] + tailleImageSurDeux;
+		By = vectPoints[ligne2 - 1][1] + tailleImageSurDeux;
+		Bz = vectPoints[ligne2 - 1][2] + tailleImageSurDeux;
+		fB = vectTexturesF[i][1];
+		vtBx = vectTexturesVt[fB - 1][0];
+		vtBy = vectTexturesVt[fB - 1][1];
+		TGAColor colorB = africanDiffuse.get(vtBx, vtBy);
+
+		ligne3 = vectTriangles[i][2];
+		Cx = vectPoints[ligne3 - 1][0] + tailleImageSurDeux;
+		Cy = vectPoints[ligne3 - 1][1] + tailleImageSurDeux;
+		Cz = vectPoints[ligne3 - 1][2] + tailleImageSurDeux;
+		fC = vectTexturesF[i][2];
+		vtCx = vectTexturesVt[fC - 1][0];
+		vtCy = vectTexturesVt[fC - 1][1];
+		TGAColor colorC = africanDiffuse.get(vtCx, vtCy);
+
+		cout << "f : " << fA << "|" << fB << "|" << fC << endl;
+		cout << "vtA : " << vtAx << "|" << vtAy << endl;
+		cout << "vtB : " << vtBx << "|" << vtBy << endl;
+		cout << "vtC : " << vtCx << "|" << vtCy << endl;
+		test.set(Ax, Ay, colorA);
+		test.set(Bx, By, colorB);
+		test.set(Cx, Cy, colorC);
+
+
+		maxAbs = maxTrois(Ax, Bx, Cx);
+		minAbs = minTrois(Ax, Bx, Cx);
+		maxOrd = maxTrois(Ay, By, Cy);
+		minOrd = minTrois(Ay, By, Cy);
+
+		double denominateur = (((Bx - Ax)*(Cy - Ay)) - ((Cx - Ax)*(By - Ay)));
+		double coeff = 1. / denominateur;
+
+		for (int i = minOrd; i <= maxOrd; i++) {
+			for (int j = minAbs; j <= maxAbs; j++) {
+				int Px = j;
+				int Py = i;
+				double u = (coeff*(Cy - Ay))*(Px - Ax) + (coeff*(Ax - Cx))*(Py - Ay);
+				double v = (coeff*(Ay - By))*(Px - Ax) + (coeff*(Bx - Ax))*(Py - Ay);
+				double w = 1 - u - v;
+				if (u < -1e-5 || v < -1e-5 || w < -1e-5) {
+				}
+				else {
+					double Pz = u*Az + v*Bz + w*Cz;
+					if (zbuffer[Px][Py] > Pz) {
+						continue;
+					}
+					else {
+						zbuffer[Px][Py] = Pz;
+						pix_x = (vtAx*u + vtBx*v + vtCx*w) * 1024;
+						pix_y = (vtAy*u + vtBy*v + vtCy*w) * 1024;
+						colorPix = africanDiffuse.get(pix_x, pix_y);
+						test.set(Px, Py, colorPix);
+					}
+				}
+			}
+		}
+	}
+
+	test.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	test.write_tga_file("test.tga");
+
+	cout << "Succes" << endl;
+}
+
 int main(int argc, char** argv) {
 	cout << "Projet d'infographie - M1 Informatique" << endl;
 	vector<vector<double> > vectPoints = lectureSommets("african_head.obj");
 	vector<vector<double> > vectTriangles = lectureTriangles("african_head.obj");
+	vector<vector<double> > vectTexturesF = lectureTexturesF("african_head.obj");
+	vector<vector<double> > vectTexturesVt = lectureTexturesVt("african_head.obj");
 	//dessinePoint(vectPoints);
 	//dessineFilDeFer(vectPoints,vectTriangles);
 	//testTriangle();
-	dessineTriangle(vectPoints, vectTriangles);
+	//dessineTriangle(vectPoints, vectTriangles);
 	//lineSweeping(vectPoints, vectTriangles);
-
+	//faceLumiere(vectPoints, vectTriangles);
+	faceTexture(vectPoints, vectTriangles, vectTexturesF, vectTexturesVt);
 	return 0;
 }
